c127ad2697db9c3058be0f217eeefe6ef13a019c
hahagarden <tingyuan413@gmail.com>
2021-08-20 23:33:14 +0900

    project_openAPI
    *VScode-File-Open Folder에서 작업폴더 들어간 후 코드 실행하면 이미지파일들이 해당 작업폴더에 저장됨.
    *저장될 폴더 지정하고 싶을 때에는 저장될 이미지 파일명에 경로지정
    (경로지정시 /로 시작하면 제일 밖으로 나와서 디렉토리 찾아감)
    *github desktop에 jpg파일 자동으로 change 올라가지 않게 하려면 .gitignore파일에 *.jpg 추가

--- full_code.py
+++ full_code.py
@@ -1,27 +1,33 @@
 import requests
 import json
 
-#이미지가 있는 image_url을 통해 file_name 파일로 저장하는 함수
+
+# 이미지가 있는 image_url을 통해 file_name 파일로 저장하는 함수
+
 
 def save_image(image_url, file_name):
-    img_response=requests.get(image_url)
-    #요청에 성공했다면
-    if img_response.status_code==200:
-        #파일저장
-        with open("file_name",'wb') as f:
+    img_response = requests.get(image_url)
+    # 요청에 성공했다면
+    if img_response.status_code == 200:
+        # 파일저장
+        with open(file_name, "wb") as f:
             f.write(img_response.content)
 
-#이미지 검색
-url="https://dapi.kakao.com/v2/search/image"
-headers={
-    "Authorization" : "KakaoAK cc335daa766cc74b3de1b1c372a6cce8"
-}
-data={
-    "query" : "펭수"
-}
 
-response=requests.post(url, headers=headers, data=data)
+# 이미지 검색
+url = "https://dapi.kakao.com/v2/search/image"
+headers = {"Authorization": "KakaoAK cc335daa766cc74b3de1b1c372a6cce8"}
+data = {"query": "사과"}
+
+response = requests.post(url, headers=headers, data=data)
 if response.status_code != 200:
-    print("errer! because ", response.json())
-else: #성공했다면,
-    count=0
+    print("error! because ", response.json())
+else:  # 성공했다면,
+    count = 0
+    for image_info in response.json()["documents"]:
+        print(f"[{count}th] image_url =", image_info["image_url"])
+        # 저장될 이미지 파일명 설정
+        count = count + 1
+        file_name = "/git/Jump2Python/temp/test_%d.jpg" % (count)
+        # 이미지 저장
+        save_image(image_info["image_url"], file_name)
